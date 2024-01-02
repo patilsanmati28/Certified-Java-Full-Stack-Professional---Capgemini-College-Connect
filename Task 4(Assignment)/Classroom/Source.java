c6bf3dd17d8d6e648a6eff6b9d4557d193a2a4cd
patilsanmati28 <134512550+patilsanmati28@users.noreply.github.com>
2024-01-02 17:28:43 +0530

    Create Source.java

---
+++ Task 4(Assignment)/Classroom/Source.java
@@ -0,0 +1,43 @@
+package com.edu;
+
+class Student{
+	String name;
+	int score;
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+	
+}
+
+class Classroom{
+	public String registerStudent (Student student) {
+		if(!student.name.equals(student.name.toUpperCase())) {
+			return "Block letters needed";
+		}
+		if (student.score < 0 || student.score > 100) {
+            return "Invalid score";
+        }
+		 return "Registered";
+	}
+	public String studentCard(String card) {
+		if(!card.matches("[0-9]+")) {
+			return "Invalid card";
+		}
+		return"Valid card";
+	}
+	
+}
+public class Main1 {
+
+	public static void main(String[] args) {
+		Student s1=new Student("A",6); 
+		Classroom cm=new Classroom();
+		System.out.println( cm.registerStudent (s1));
+		 
+		 
+
+	}
+
+}
